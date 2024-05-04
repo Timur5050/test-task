ad9c5e98817ca825ad3212039123f68162508239
Timur <stukantimur811@gmail.com>
2024-05-04 09:20:06 +0300

    created serializer and some http request mmrthods

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -1,9 +1,14 @@
+import io
+
 from rest_framework import serializers
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+
 from .models import Restaurant
 
 
-class RestaurantSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Restaurant
-        fields = ('id_restaurant', 'name_restaurant')
+class RestaurantSerializer(serializers.Serializer):
+    id_restaurant = serializers.IntegerField()
+    name_restaurant = serializers.CharField(max_length=255)
+    menu_update_time = serializers.DateTimeField(read_only=True)
 
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,4 @@
+from django.forms import model_to_dict
 from rest_framework import generics
 from django.shortcuts import render
 from rest_framework.response import Response
@@ -14,5 +15,15 @@ from .serializers import RestaurantSerializer
 #     serializer_class = RestaurantSerializer
 class RestaurantAPIView(APIView):
     def get(self, request):
-        return Response({'restaurant_id': '1'})
+        lst = Restaurant.objects.all()
+        return Response({'restaurants': RestaurantSerializer(lst, many=True).data})
 
+    def post(self, request):
+        serializer = RestaurantSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        restaurant_new = Restaurant.objects.create(
+            id_restaurant=request.data['id_restaurant'],
+            name_restaurant=request.data['name_restaurant'],
+        )
+        return Response({'restaurant': RestaurantSerializer(restaurant_new).data})
